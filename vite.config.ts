e80f3815c3e274b09d164a9388d7fcf063aca7e6
AnChekhov <andrey.xpost@gmail.com>
2025-12-05 12:59:44 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // Самая важная строка, исправляющая белый экран:
       base: '/ztppv5.github.io/',
-      
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ""),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || "")
       },
       resolve: {
         alias: {
